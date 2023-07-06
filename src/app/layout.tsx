c06047f04968c25db45a1a6bc3eeb03010aba547
Yab <yabuman@gmail.com>
2023-07-06 18:56:56 +0300

    working on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 
@@ -9,8 +10,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className={cn("bg-white text-slate-900 antialiased", inter.className)} lang="en">
+      <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
