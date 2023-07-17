4306f8cb2bfba9426266cedea7347bde6ed079c4
Yagth <yabuman46@gmail.com>
2023-07-17 18:51:02 +0300

    Removed the gradient effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
-import Image from "next/image";
-import Paragraph from "@/ui/Paragraph";
+import type { Metadata } from "next";
+import LargeHeading from "@/ui/LargeHeading";
+
+export const metadata: Metadata = {
+  title: "Similarity API | Home",
+  description: "Free and open-source text similarity",
+};
 
 export default function Home() {
-  return <main className="bg-red-500">Hello</main>;
+  return (
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+      <div className="container pt-32 max-w-7xl mx-auto w-full h-full">
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
+          <LargeHeading
+            size="lg"
+            className="three-d text-black dark:text-light-gold"
+          ></LargeHeading>
+        </div>
+      </div>
+    </div>
+  );
 }
