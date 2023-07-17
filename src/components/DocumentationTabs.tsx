315726a2d21f0c43e53939002ade21018d5d221a
Yagth <yabuman46@gmail.com>
2023-07-17 19:56:38 +0300

    Created a code componenet

--- src/components/DocumentationTabs.tsx
+++ src/components/DocumentationTabs.tsx
@@ -1,7 +1,21 @@
 import { FC } from "react";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/ui/Tabs";
+import SimpleBar from "simplebar-react";
+import Code from "@/components/Code";
 
 const DocumentationTabs: FC<> = () => {
-  return <div>DocumentationTabs</div>;
+  return (
+    <Tabs defaultValue="nodejs" className="max-w-2xl w-full">
+      <TabsList>
+        <TabsTrigger value="nodejs">NodeJS</TabsTrigger>
+        <TabsTrigger value="python">Python</TabsTrigger>
+      </TabsList>
+      <TabsContent value="nodejs">
+        <Code language="javascript" code="" />
+      </TabsContent>
+      <TabsContent value="python"></TabsContent>
+    </Tabs>
+  );
 };
 
 export default DocumentationTabs;
