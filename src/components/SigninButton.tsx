ba15f0f73c96d00903dff7b20324dc0ceec46b02
Yagth <yabuman46@gmail.com>
2023-07-17 11:53:03 +0300

    Working on sign in / out button

---
+++ src/components/SigninButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { FC, useState } from "react";
+import Button from "@/ui/Button";
+import { signIn } from "next-auth/react";
+
+interface SigninButtonProps {}
+
+const SigninButton: FC<SigninButtonProps> = ({}) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const signInWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn("google");
+    } catch (error) {
+      //   toast({
+      //     title: "Error signing in",
+      //     messge: "Please try again later",
+      //     type: "Error",
+      //   });
+    }
+  };
+
+  return (
+    <Button onClick={signInWithGoogle} isLoading={isLoading}>
+      Sign In
+    </Button>
+  );
+};
+
+export default SigninButton;
