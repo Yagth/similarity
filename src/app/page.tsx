244ce8d1ce5a904f962caba88d319448c31308e6
Yagth <yabuman46@gmail.com>
2023-07-17 19:02:28 +0300

    Working on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import LargeHeading from "@/ui/LargeHeading";
+import Paragraph from "@/ui/Paragraph";
+import Link from "next/link";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Similarity API | Home",
@@ -14,7 +17,31 @@ export default function Home() {
           <LargeHeading
             size="lg"
             className="three-d text-black dark:text-light-gold"
-          ></LargeHeading>
+          >
+            Easily determine <br /> text similarity
+          </LargeHeading>
+          <Paragraph className="max-w-xl lg:text-left">
+            WIth the text similarity API, you can easily determine teh
+            similarity between two pieces of text with a free{" "}
+            <Link
+              href="/login"
+              className="underline underline-offset-2 text-black dark:text-light-gold"
+            >
+              API key
+            </Link>
+            .
+          </Paragraph>
+          <div className="relative w-full max-w-lg lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute">
+            <Image
+              priority
+              className="img-shadow"
+              quality={100}
+              style={{ objectFit: "contain" }}
+              fill
+              src="/typewriter.png"
+              alt="typewriter"
+            />
+          </div>
         </div>
       </div>
     </div>
