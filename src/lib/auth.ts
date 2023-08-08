21cba4866be6928e1f7c6b231abe4458c80a60f0
Yagth <yabuman46@gmail.com>
2023-08-08 12:25:16 +0300

    Changed the auth.ts to teh default one

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,74 +1,69 @@
-import { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { db } from '@/lib/db'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
-function getGoogleCredentials() {
-  const clientId = process.env.GOOGLE_CLIENT_ID;
-  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-
+function getGoogleCredentials(): { clientId: string; clientSecret: string } {
+  const clientId = process.env.GOOGLE_CLIENT_ID
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET
   if (!clientId || clientId.length === 0) {
-    throw new Error("No clientID for google provider set");
+    throw new Error('Missing GOOGLE_CLIENT_ID')
   }
 
   if (!clientSecret || clientSecret.length === 0) {
-    throw new Error("No clientSecret for google provider set");
+    throw new Error('Missing GOOGLE_CLIENT_SECRET')
   }
 
-  return { clientId, clientSecret };
+  return { clientId, clientSecret }
 }
 
-let authOptions: NextAuthOption;
-if (typeof window === "undefined") {
-  const { PrismaAdapter } = require("@next-auth/prisma-adapter");
-  const { db } = require("@/lib/db");
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
 
-  authOptions = {
-    session: {
-      strategy: "jwt",
+      return session
     },
-    pages: {
-      signIn: "/login",
-    },
-    callbacks: {
-      session({ token, session }) {
-        if (token) {
-          session.user.id = token.id;
-          session.user.name = token.name;
-          session.user.email = token.email;
-          session.user.image = token.picture;
-        }
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
 
-        return session;
-      },
-      async jwt({ token, user }) {
-        const dbuser = await db.user.findFirst({
-          where: {
-            email: "",
-          },
-        });
+      if (!dbUser) {
+        token.id = user!.id
+        return token
+      }
 
-        if (!dbuser) {
-          token.id = user!.id; // ! used to let typescript know that we know the variable user exists and it shouldn't complain about it
-          return token;
-        }
-        return {
-          id: dbuser.id,
-          name: dbuser.name,
-          email: dbuser.email,
-          picture: dbuser.image,
-        };
-      },
-      redirect() {
-        return "/dashboard";
-      },
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
     },
-    adapter: PrismaAdapter(db),
-    providers: [
-      GoogleProvider({
-        clientId: getGoogleCredentials().clientId,
-        clientSecret: getGoogleCredentials().clientSecret,
-      }),
-    ],
-  };
+    redirect() {
+      return '/dashboard'
+    },
+  },
 }
-
-export { authOptions };
